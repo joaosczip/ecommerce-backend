427a3fe027594fdf0fb19cac988d7554ca58fdb7
João Guilherme <joao.sczip@hotmail.com>
2025-01-30 09:02:09 -0300

    chore: add typeorm and setup db module

---
+++ packages/orders-service/src/modules/database/database.provider.ts
@@ -0,0 +1,21 @@
+import { DataSource } from "typeorm";
+
+export const databaseProviders = [
+	{
+		provide: "MAIN_DATABASE",
+		useFactory: async () => {
+			const dataSource = new DataSource({
+				type: "postgres",
+				host: "localhost",
+				port: 5432,
+				username: "root",
+				password: "root",
+				database: "test",
+				entities: [__dirname + "./src/models/*.ts"],
+				synchronize: true,
+			});
+
+			return dataSource.initialize();
+		},
+	},
+];
